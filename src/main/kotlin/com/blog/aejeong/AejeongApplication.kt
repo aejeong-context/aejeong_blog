c61323fb48e66c8e904c717de3b8bbebfd70d03a
aejeong <aejeong-context@kakao.com>
2023-03-08 12:43:10 +0900

    chore: 프로젝트 세팅
    기본적인 spring-kotlin 프로젝트 설정

---
+++ src/main/kotlin/com/blog/aejeong/AejeongApplication.kt
@@ -0,0 +1,11 @@
+package com.blog.aejeong
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AejeongApplication
+
+fun main(args: Array<String>) {
+    runApplication<AejeongApplication>(*args)
+}
---
+++ src/test/kotlin/com/blog/aejeong/AejeongApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blog.aejeong
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AejeongApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
